15516139c755af037db77e2d9b9ea6373867a77c
mahesh-143 <maheshodedara13@yahoo.com>
2024-02-24 20:12:16 +0530

    adds quiz logic

--- src/routes/quiz.tsx
+++ src/routes/quiz.tsx
@@ -1,4 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { hiragana } from "../utils/hiragana.ts";
+
+interface Kana {
+  roumaji: string;
+  kana: string;
+}
+
 export const Quiz = () => {
-  return <div>Quiz route</div>;
+  const [questions, setQuestions] = useState<Kana[]>([]);
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+
+  const getQuestion = () => {
+    const shuffledKanaData = [...hiragana].sort(() => Math.random() - 0.5);
+    const allQuestions = shuffledKanaData;
+    setQuestions(allQuestions);
+  };
+
+  const getCorrectAnswer = (): string => {
+    return questions[currentQuestion]?.roumaji;
+  };
+
+  const handleAnswer = (selectedOption: string | undefined) => {
+    if (selectedOption === getCorrectAnswer()) {
+      setScore((prevScore) => prevScore + 1);
+    }
+    setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+  };
+
+  const getShuffledOptions = () => {
+    const incorrectOptions = hiragana
+      .filter((kana) => kana?.roumaji !== getCorrectAnswer())
+      .map((kana) => kana?.roumaji);
+
+    const randomIncorrectOptions = incorrectOptions
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 3);
+
+    const options = [getCorrectAnswer(), ...randomIncorrectOptions];
+
+    return options.sort(() => Math.random() - 0.5);
+  };
+
+  useEffect(() => {
+    getQuestion();
+  }, []);
+  return (
+    <div>
+      {questions.length > 0 && currentQuestion < questions.length ? (
+        <div>
+          <p>{questions[currentQuestion].kana}</p>
+          <ul>
+            {getShuffledOptions().map((option, index) => (
+              <li key={index} onClick={() => handleAnswer(option)}>
+                {option}
+              </li>
+            ))}
+          </ul>
+          <p>score:{score}</p>
+        </div>
+      ) : (
+        <p>
+          {questions.length > 0 ? "Quiz completed!" : "Loading questions..."}
+        </p>
+      )}
+    </div>
+  );
 };
