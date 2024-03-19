ad3fa7d0c98801fa67e081775d738631e99a2cf7
mahesh-143 <maheshodedara13@yahoo.com>
2024-03-19 18:07:16 +0530

    adds active link states

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, HStack, Link, Text, css } from "@kuma-ui/core";
+import { Button, HStack, Link, Text } from "@kuma-ui/core";
 import { NavLink as ReactLink } from "react-router-dom";
 import { Logo } from "./ui/Logo";
 import { MdMenu } from "react-icons/md";
@@ -12,7 +12,7 @@ type NavLinksTypes = {
 
 const navLinks = [
   { href: "/", key: "Home", text: "Home" },
-  { href: "/quiz", key: "Quiz", text: "Quiz" },
+  { href: "/quiz/all", key: "Quiz", text: "Quiz" },
 ];
 
 const NavLink = ({ href, text }: NavLinksTypes) => {
@@ -30,16 +30,16 @@ const NavLink = ({ href, text }: NavLinksTypes) => {
         textUnderlineOffset: "5px",
         textDecorationColor: "colors.accent2",
       }}
-      // @ts-ignore
-      className={({ isActive }) => {
-        console.log(isActive);
-        isActive
-          ? css`
-              color: red;
-            `
-          : css`
-              color: blue;
-            `;
+      // there is nothing we can do - Napolean
+      style={({ isActive }: any) => {
+        return isActive
+          ? {
+              textDecoration: "underline",
+              textDecorationThickness: "3px",
+              textUnderlineOffset: "5px",
+              textDecorationColor: "#6B8E23",
+            }
+          : {};
       }}
     >
       <Text>{text}</Text>
