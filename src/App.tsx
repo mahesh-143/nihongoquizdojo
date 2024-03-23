fb58eba005a5da7d59592808fdfb45c968c28a0d
mahesh-143 <maheshodedara13@yahoo.com>
2024-03-23 20:16:40 +0530

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar } from "./components/Navbar";
 import { Outlet } from "react-router";
 import { Box } from "@kuma-ui/core";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Box px={16} maxWidth="1280px" marginX="auto" fontFamily="fonts.body">
         <Outlet />
       </Box>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, Text } from "@kuma-ui/core";
+
+export function Footer() {
+  return (
+    <Box
+      background="#F3F4F6"
+      height="75px"
+      display="flex"
+      alignItems="center"
+      fontFamily="fonts.body"
+    >
+      <Text textAlign="center" margin="auto">
+        © 2024 <a href="https://github.com/mahesh-143">Mahesh Odedara</a>. All
+        rights reserved.
+      </Text>
+    </Box>
+  );
+}
+
+export default Footer;
